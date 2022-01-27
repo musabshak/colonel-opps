67a94874eec0da263e0c7815a58a0a5a1b308127
Varun Malladi <varun.malladi.23@dartmouth.edu>
2022-01-26 19:56:40 -0500

    trap skeletons

--- kernel_data_structs.c
+++ kernel_data_structs.c
@@ -3,6 +3,13 @@
  *      - process
  *          - user stack, heap
  *          - kernel stack, heap, data, text
+ *      - page (logical memory), frame (physical memory)
+ *          - page table entry (pte) sketched in include/hardware.h
+ *      - user context
+ *          - include/hardware.h
+ *      - kernel context
+ *          - include/hardware.h
+ *      - syscall table
  *      - array of active processes
  *      - queue of waiting processes
  *      - array of exit statuses, to be checked for waiting process
@@ -17,27 +24,61 @@
  *      - file?
  * 
  *      - interrupt vector, vector table
- * 
+ */
+
+/* 
  * ===============
  * === PROCESS === 
  * ===============
- *  typdef struct Process {
- *      // --- userland 
- *      user_stack;
- *      user_heap;
- *      user_data;
- *      user_text;
- *      // --- kernelland
- *      kernel stack;
- *      kernel_heap;
- *      kernel_data;
- *      kernel_text;
- *      // --- metadata
- *      process_t *parent;
- *      int pid;
- *      bool initialized; // to see if we own proc mem or its just copied from fork()
- *  } process_t;
  * 
+ */
+
+typedef struct Process {
+    // --- userland 
+    user_stack;
+    user_heap;
+    user_data;
+    user_text;
+    // --- kernelland
+    kernel stack;
+    kernel_heap;
+    kernel_data;
+    kernel_text;
+    // --- metadata
+    process_t *parent;
+    int pid;
+} process_t;
+
+
+/*
+ *  =============
+ *  === FRAME === 
+ *  =============
+ * 
+ *  Copy-On-Write:
+ *      - Each frame keeps track of how many page tables it appears in (ref_counter).
+ *      When something like fork() is called, we use COW on the child process's 
+ *      memory and page table, meaning we point to the same physical frames (as 
+ *      opposed to copying the parent's data to a new frame).
+ *      - Crucially, when we fork() we increment each frame in the page table's 
+ *      rec_counter. Whenever we try to write to a frame, the kernel first checks if 
+ *      the number of references to it is 1. If so, it is not being shared and it is
+ *      safe to write. 
+ *      - If the references are >1, then the frame is shared and it is not safe to 
+ *      write. The kernel copies the current frame to a new frame, replaces the old 
+ *      frame in the (calling process's) page table with the new frame, and decrements 
+ *      the reference counter.
+ * 
+ */
+
+typedef struct Frame {
+    int ref_counter;
+    int id;
+    int base;
+    int limit;
+} frame_t;
+
+/* 
  * ========================
  * === ACTIVE PROCESSES ===
  * ========================
---
+++ trap_handlers.c
@@ -0,0 +1,148 @@
+
+/*
+ *  ===================
+ *  === TRAP KERNEL ===
+ *  ===================
+ * 
+ *  OS response (manual, p. 36):
+ *      Execute the requested syscall, as indicated by the syscall number 
+ *      in the code field of the UserContext passed by reference to this 
+ *      trap handler function.
+ *      The arguments to the syscall will be found in the registers, starting 
+ *      with regs[0].
+ *      The return value from the syscall should be returned to the user process 
+ *      in the regs[0] field of the UserContext.
+ * 
+ *  - The code numbers are found in include/yalnix.h
+ * 
+ */
+
+int TrapKernelHandler(UserContext *user_context) {
+    int syscall_code = user_context->code;    
+    void **args = user_context->regs;
+
+    // Locate syscall in syscall table
+    // Invoke syscall with args    
+}
+
+/*
+ *  =================
+ *  === TRAPCLOCK ===
+ *  =================
+ * 
+ *  OS response (manual, p. 36):
+ *      If there are other runnable processes on the ready queue, perform 
+ *      a context switch to the next runnable process. (The Yalnix kernel 
+ *      should implement round-robin process scheduling with a CPU quantum 
+ *      per process of 1 clock tick.)
+ *      If there are no runnable processes, dispatch idle.
+ * 
+ */
+
+int TrapClock(UserContext *user_context) {
+
+}
+
+/*
+ *  ===================
+ *  === TRAPILLEGAL ===
+ *  ===================
+ * 
+ *  OS response (manual, p. 36):
+ *      Abort the currently running Yalnix user process but continue running 
+ *      other processes.
+ * 
+ */
+
+int TrapIllegal(UserContext *user_context) {
+
+}
+
+/*
+ *  ==================
+ *  === TRAPMEMORY ===
+ *  ==================
+ * 
+ *  OS response (manual, p. 36):
+ *      The kernel must determine if this exception represents an implicit 
+ *      request by the current process to enlarge the amount of memory allocated 
+ *      to the process’s stack. If so, the kernel enlarges the process’s stack 
+ *      to “cover” the address that was being referenced that caused the exception 
+ *      (the addr field in the UserContext) and then returns from the exception, 
+ *      allowing the process to continue execution with the larger stack. (For 
+ *      more discussion, see Section 3.5.2 below.)
+ *      Otherwise, abort the currently running Yalnix user process but continue 
+ *      running other processes.
+ * 
+ */
+
+int TrapMemory(UserContext *user_context) {
+
+}
+
+/*
+ *  ================
+ *  === TRAPMATH ===
+ *  ================
+ * 
+ *  From manual (p. 36):
+ *
+ */
+
+int TrapMath(UserContext *user_context) {
+
+}
+
+/*
+ *  ======================
+ *  === TRAPTTYRECEIVE ===
+ *  ======================
+ * 
+ *  OS response (manual, p. 36):
+ *      This interrupt signifies that a new line of input is available 
+ *      from the terminal indicated by the code field in the UserContext 
+ *      passed by reference to this interrupt handler func- tion. The 
+ *      kernel should read the input from the terminal using a TtyReceive 
+ *      hardware operation and if necessary buffer the input line for a 
+ *      subsequent TtyRead syscall by some user process.
+ * 
+ */
+
+int TrapTTYReceive(UserContext *user_context) {
+
+}
+
+/*
+ *  =======================
+ *  === TRAPTTYTRANSMIT ===
+ *  =======================
+ * 
+ *  OS response (manual, p. 36):
+ *      This interrupt signifies that a previous TtyTransmit hardware 
+ *      operation on some terminal has completed. The specific terminal is 
+ *      indicated by the code field in the UserContext passed by reference 
+ *      to this interrupt handler function. The kernel should complete the 
+ *      blocked process that started this terminal output from a TtyWrite 
+ *      syscall, as necessary; also start the next terminal output on this 
+ *      terminal, if any.
+ * 
+ */
+
+int TrapTTYTransmit(UserContext *user_context) {
+
+}
+
+/*
+ *  ================
+ *  === TRAPDISK ===
+ *  ================
+ * 
+ *  OS response (manual, p. 36):
+ *      Your OS can ignore these traps, unless you’ve decided to implement 
+ *      extra functionality involving the disk.
+ * 
+ */
+
+int TrapDisk(UserContext *user_context) {
+
+}
