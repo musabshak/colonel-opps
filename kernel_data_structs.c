9163c87fa2ac8cf202cf80c90221f8c429d58bdb
Musab Shakeel <musabshakeel@gmail.com>
2022-01-27 10:47:09 -0500

    minor formatting change; ready to write pseudocode for syscalls

--- kernel_data_structs.c
+++ kernel_data_structs.c
@@ -19,28 +19,28 @@
  *      - cvar (condition variable)
  *      - file descriptor
  *          - file descriptor table (per process) ->
- *          - file table (system wide, indexes opened files with mode) -> 
+ *          - file table (system wide, indexes opened files with mode) ->
  *          - inode table (system wide)
  *      - file?
- * 
+ *
  *      - interrupt vector, vector table
  */
 
-/* 
+/*
  * ===============
- * === PROCESS === 
+ * === PROCESS ===
  * ===============
- * 
+ *
  */
 
 typedef struct Process {
-    // --- userland 
+    // --- userland
     user_stack;
     user_heap;
     user_data;
     user_text;
     // --- kernelland
-    kernel stack;
+    kernel_stack;
     kernel_heap;
     kernel_data;
     kernel_text;
@@ -49,26 +49,25 @@ typedef struct Process {
     int pid;
 } process_t;
 
-
 /*
  *  =============
- *  === FRAME === 
+ *  === FRAME ===
  *  =============
- * 
+ *
  *  Copy-On-Write:
  *      - Each frame keeps track of how many page tables it appears in (ref_counter).
- *      When something like fork() is called, we use COW on the child process's 
- *      memory and page table, meaning we point to the same physical frames (as 
+ *      When something like fork() is called, we use COW on the child process's
+ *      memory and page table, meaning we point to the same physical frames (as
  *      opposed to copying the parent's data to a new frame).
- *      - Crucially, when we fork() we increment each frame in the page table's 
- *      rec_counter. Whenever we try to write to a frame, the kernel first checks if 
+ *      - Crucially, when we fork() we increment each frame in the page table's
+ *      rec_counter. Whenever we try to write to a frame, the kernel first checks if
  *      the number of references to it is 1. If so, it is not being shared and it is
- *      safe to write. 
- *      - If the references are >1, then the frame is shared and it is not safe to 
- *      write. The kernel copies the current frame to a new frame, replaces the old 
- *      frame in the (calling process's) page table with the new frame, and decrements 
+ *      safe to write.
+ *      - If the references are >1, then the frame is shared and it is not safe to
+ *      write. The kernel copies the current frame to a new frame, replaces the old
+ *      frame in the (calling process's) page table with the new frame, and decrements
  *      the reference counter.
- * 
+ *
  */
 
 typedef struct Frame {
@@ -78,51 +77,56 @@ typedef struct Frame {
     int limit;
 } frame_t;
 
-/* 
+/*
  * ========================
  * === ACTIVE PROCESSES ===
  * ========================
- *  typedef struct ActiveProcesses {
- *      process_t *procs;
- *      int size;
- *  } active_procs_t;
- * 
- * =========================
+ */
+typedef struct ActiveProcesses {
+    process_t *procs;
+    int size;
+} active_procs_t;
+
+/** =========================
  * === WAITING PROCESSES ===
  * =========================
- *  typedef struct WaitingProcesses {
- *      
- *  } waiting_procs_t;
- * 
- * ====================
+ */
+typedef struct WaitingProcesses {
+
+} waiting_procs_t;
+
+/** ====================
  * === KERNEL STACK ===
  * ====================
- *  typedef struct KernelStack {
- *      size;
- *      base;
- *      limit;
- *      frame_ptr;
- *      stack_ptr;
- *  } ker_stack_t;
- * 
+ *
+ *
  *  - this will be unique to each process
  *  - size is fixed in this implementation
  *  - do we need size? does keeping it give extra safety and/or efficiency?
- * 
+ *
  *  --- Methods:
  *  ker_stack_t *ker_stack_new();   // allocate enough space, return pointer
  *                                  // relative to virtual memory of calling process
- *
- * ===================
+ */
+typedef struct KernelStack {
+    size;
+    base;
+    limit;
+    frame_ptr;
+    stack_ptr;
+} ker_stack_t;
+
+/** ===================
  * === KERNEL HEAP ===
  * ===================
- *  typedef struct KernelHeap {
- *      brk;
- *      size;
- *      base;
- *      limit;
- *  } ker_heap_t;
- * 
+
+ *
  *  --- Methods:
- *  
+ *
  */
+typedef struct KernelHeap {
+    brk;
+    size;
+    base;
+    limit;
+} ker_heap_t;
--- syscalls/sync_syscalls.c
+++ syscalls/sync_syscalls.c
@@ -2,120 +2,112 @@
  *  ================
  *  === LOCKINIT ===
  *  ================
- * 
+ *
  *  From manual (p. 34):
- *      Create a new lock; save its identifier at *lock idp. In case 
+ *      Create a new lock; save its identifier at *lock idp. In case
  *      of any error, the value ERROR is returned.
- * 
+ *
  */
 
 int kLockInit(int *lock_idp) {
-
 }
 
 /*
  *  ===============
  *  === ACQUIRE ===
  *  ===============
- * 
+ *
  *  From manual (p. 34):
- *      Acquire the lock identified by lock id. In case of any error, 
+ *      Acquire the lock identified by lock id. In case of any error,
  *      the value ERROR is returned.
- * 
+ *
  */
 
 int kAcquire(int lock_id) {
-
 }
 
 /*
  *  ===============
  *  === RELEASE ===
  *  ===============
- * 
+ *
  *  From manual (p. 35):
- *      Release the lock identified by lock id. The caller must currently 
+ *      Release the lock identified by lock id. The caller must currently
  *      hold this lock. In case of any error, the value ERROR is returned.
- * 
+ *
  */
 
 int kRelease(int lock_id) {
-
 }
 
 /*
  *  ================
  *  === CVARINIT ===
  *  ================
- * 
+ *
  *  From manual (p. 35):
- *      Create a new condition variable; save its identifier at *cvar_idp. 
+ *      Create a new condition variable; save its identifier at *cvar_idp.
  *      In case of any error, the value ERROR is returned.
- * 
+ *
  */
 int kCvarInit(int *cvar_idp) {
-
 }
 
 /*
  *  ==================
  *  === CVARSIGNAL ===
  *  ==================
- * 
+ *
  *  From manual (p. 35):
- *      Signal the condition variable identified by cvar id. (Use Mesa-style 
+ *      Signal the condition variable identified by cvar id. (Use Mesa-style
  *      semantics.) In case of any error, the value ERROR is returned.
- * 
+ *
  */
 int kCvarSignal(int cvar_id) {
-
 }
 
 /*
  *  =====================
  *  === CVARBROADCAST ===
  *  =====================
- * 
+ *
  *  From manual (p. 35):
- *      Broadcast the condition variable identified by cvar id. (Use Mesa-style 
+ *      Broadcast the condition variable identified by cvar id. (Use Mesa-style
  *      semantics.) In case of any error, the value ERROR is returned.
- * 
+ *
  */
 int kCvarBroadcast(int cvar_id) {
-
 }
 
 /*
  *  ================
  *  === CVARWAIT ===
  *  ================
- * 
+ *
  *  From manual (p. 35):
- *      The kernel-level process releases the lock identified by lock id and 
- *      waits on the condition variable indentified by cvar id. When the 
- *      kernel-level process wakes up (e.g., because the condition variable was 
+ *      The kernel-level process releases the lock identified by lock id and
+ *      waits on the condition variable indentified by cvar id. When the
+ *      kernel-level process wakes up (e.g., because the condition variable was
  *      signaled), it re-acquires the lock. (Use Mesa-style semantics.)
- *      When the lock is finally acquired, the call returns to userland. In case 
+ *      When the lock is finally acquired, the call returns to userland. In case
  *      of any error, the value ERROR is returned.
- * 
+ *
  */
 int kCvarWait(int cvar_id, int lock_id) {
-
 }
 
 /*
  *  ===============
  *  === RECLAIM ===
  *  ===============
- * 
+ *
  *  From manual (p. 35):
- *      Destroy the lock, condition variable, or pipe indentified by id, 
+ *      Destroy the lock, condition variable, or pipe indentified by id,
  *      and release any associated resources.
  *      In case of any error, the value ERROR is returned.
- *      If you feel additional specification is necessary to handle unusual 
+ *      If you feel additional specification is necessary to handle unusual
  *      scenarios, then create and document it.
- * 
+ *
  */
 int kReclaim(int id) {
-
 }
