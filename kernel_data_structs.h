408f058f7e1b6ce7231a87d18d72775ef3dd066f
Varun Malladi <varun.malladi.23@dartmouth.edu>
2022-01-31 21:55:52 -0500

    add headers

---
+++ kernel_data_structs.h
@@ -0,0 +1,36 @@
+#ifndef __KERNEL_DATA_STRUCTS
+#define __KERNEL_DATA_STRUCTS
+
+//  ===== GLOBAL DATA =====
+queue_t *READY_PROCESSES;
+queue_t *BLOCKED_PROCESSES;
+queue_t *ZOMBIE_PROCESSES;
+
+//  ===== PROCESS CONTROL BLOCK =====
+typedef struct ProcessControlBlock pcb_t;
+
+pcb_t *pcb_copy(pcb_t *pcb_tocopy);
+
+//  ===== PAGE TABLE =====
+const int NUM_PT_ENTRIES;
+
+typedef struct PageTable pagetable_t;
+
+pagetable_t *pagetable_new(); 
+pagetable_t *pagetable_deepcopy();
+pagetable_t *pagetable_newcopy(pagetable_t *callers_pt);
+pagetable_t *pagetable_deepcopy(pagetable_t *pagetable_tocopy);
+
+//  ===== FRAME, FRAME TABLE =====
+typedef struct Frame frame_t;
+typedef struct FrameTable frametable_t;
+
+frame_t frame_new(int ref_count, int id, int base, int limit);
+frametable_t frametable_init(unsigned int hardware_mem_size, int frame_size, int pmem_base);
+int find_free_frame();
+
+//  ===== KERNEL STUFF =====
+typedef struct KernelStack ker_stack_t;
+typedef struct KernelHeap ker_heap_t;
+
+#endif // __KERNEL_DATA_STRUCTS
